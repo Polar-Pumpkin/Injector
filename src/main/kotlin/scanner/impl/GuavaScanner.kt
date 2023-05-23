ee84b903deca127f6efa886cd8feb7d5c2dd710e
Legoshi <entityparrot@qq.com>
2023-05-23 22:23:14 +0800

    Add an option to control GuavaScanner determine to exclude internal package name or not

--- src/main/kotlin/scanner/impl/GuavaScanner.kt
+++ src/main/kotlin/scanner/impl/GuavaScanner.kt
@@ -5,7 +5,7 @@ import com.google.common.reflect.ClassPath
 import scanner.Scanner
 
 @Suppress("MemberVisibilityCanBePrivate", "unused")
-class GuavaScanner(val external: Class<*>) : Scanner {
+class GuavaScanner(val external: Class<*>, val excludeInternal: Boolean = true) : Scanner {
     override fun scan(): List<Class<*>> {
         val domain = external.packageName
         val internal = ProjectFactory::class.java.packageName
@@ -13,7 +13,7 @@ class GuavaScanner(val external: Class<*>) : Scanner {
             .allClasses
             .stream()
             .filter { it.packageName.startsWith(domain) }
-            .filter { !it.packageName.startsWith(internal) }
+            .filter { excludeInternal && !it.packageName.startsWith(internal) }
             .map { it.load() }
             .toList()
     }
